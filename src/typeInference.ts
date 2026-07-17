c45ffc273c221aa2693ea227ee6f5e38d6736571
rubayatrifat <rubayatislam17@gmail.com>
2026-07-17 15:31:15 +0600

    Type Inference TypeScript

---
+++ src/typeInference.ts
@@ -0,0 +1,12 @@
+// Type Inference
+
+let name = "Rbayat Rifat" // Automatically choses to be sting
+
+let strOrNum = Math.random() > 0.5 ? 10 : "nothing" // Automatically choses 2 data types
+
+
+// Type annotaions
+
+let userRole: string = "Backend Developer"
+userRole = "System Designer"
+
