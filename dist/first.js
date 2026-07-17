5e02752d3d05f2474228e6bbb5e8b2c5a636f169
rubayatrifat <rubayatislam17@gmail.com>
2026-07-17 14:16:42 +0600

    Setup TS & write first code

---
+++ dist/first.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `Hello ${name},, Happy Coding!!`;
+}
+const userName = "Rubayat Rifat";
+console.log(greet(userName));
+//# sourceMappingURL=first.js.map
---
+++ src/first.ts
@@ -0,0 +1,6 @@
+function greet(name: string) {
+    return `Hello ${name},, Happy Coding!!`
+}
+
+const userName: string = "Rubayat Rifat"
+console.log(greet(userName))
