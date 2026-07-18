95208cc0686784e843c54c05f19d3cdb1450a0bd
rubayatrifat <rubayatislam17@gmail.com>
2026-07-18 22:54:40 +0600

    Classes TypeScript

--- dist/classes-objects.js
+++ dist/classes-objects.js
@@ -1,16 +1,35 @@
 "use strict";
+/// TypeScript Classes
 Object.defineProperty(exports, "__esModule", { value: true });
 class CreateUser {
     name;
     role;
     age;
-    constructor(name, role, age) {
+    id;
+    constructor(name, role, age, id) {
         this.name = name;
         this.role = role;
         this.age = age;
+        this.id = id;
+    }
+    showId() {
+        return `${this.name}'s ID is ${this.id}`; // Id is accessable to a method
     }
 }
-let userOne = new CreateUser("Rubayat Rifat", "System Designer", 26);
-let userTwo = new CreateUser("Manush", "Artist", 36);
+let userOne = new CreateUser("Rubayat Rifat", "System Designer", 26, "7D4U5");
+let userTwo = new CreateUser("Manush", "Artist", 36, "9LK2L");
+// userOne.id = "something" --- ID is not accesable
 console.log(userOne, userTwo);
+console.log(userOne.showId(), userTwo.showId());
+// Extends
+class CreateAdmin extends CreateUser {
+    deletePer;
+    constructor(name, role, age, id, deletePer) {
+        super(name, role, age, id);
+        this.deletePer = deletePer;
+        this.deletePer = true;
+    }
+}
+let admin = new CreateAdmin("Admin Rifat", "System Designer", 26, "7D4U5");
+console.log(admin);
 //# sourceMappingURL=classes-objects.js.map
--- src/classes-objects.ts
+++ src/classes-objects.ts
@@ -1,12 +1,34 @@
+/// TypeScript Classes
+
 class CreateUser {
   constructor(
     public name: string,
     public role: string,
     public age: number,
+    private id: string,
   ) {}
+
+  showId() {
+    return `${this.name}'s ID is ${this.id}`        // Id is accessable to a method
+  }
+
 }
 
-let userOne = new CreateUser("Rubayat Rifat", "System Designer", 26);
-let userTwo = new CreateUser("Manush", "Artist", 36);
+let userOne = new CreateUser("Rubayat Rifat", "System Designer", 26, "7D4U5");
+let userTwo = new CreateUser("Manush", "Artist", 36, "9LK2L");
+
+// userOne.id = "something" --- ID is not accesable
 
 console.log(userOne, userTwo);
+console.log(userOne.showId(), userTwo.showId())
+
+// Extends
+class CreateAdmin extends CreateUser {
+    constructor (name: string, role: string, age: number, id: string, public deletePer?: boolean) {
+        super(name, role, age, id)
+        this.deletePer = true
+    }
+}
+
+let admin = new CreateAdmin("Admin Rifat", "System Designer", 26, "7D4U5");
+console.log(admin)
